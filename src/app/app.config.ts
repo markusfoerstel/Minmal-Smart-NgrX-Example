6ad70098de1edda295376a885c9238d510c77d73
Sniqi <sniqi.dev@gmail.com>
2025-06-10 13:17:43 +0200

    importProvidersFrom fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideStore, StoreModule } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { encounterEffectsServiceToken } from './encounter/encounter-effects.service-token';
 import { EncounterEffectsService } from './encounter/encountereffects.service';
 import { provideSmartNgRX } from '@smarttools/smart-ngrx';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection(), 
+    provideZoneChangeDetection(),
     {
       provide: encounterEffectsServiceToken,
       useClass: EncounterEffectsService,
     },
     provideHttpClient(),
+    importProvidersFrom(
+      StoreModule.forRoot({}),
+    ),
     provideStore(),
     provideEffects(),
     provideSmartNgRX({}),
     provideRouter(routes),
+    provideStoreDevtools({
+        maxAge: 25, // Retains last 25 states
+        logOnly: false, // Restrict extension to log-only mode
+        autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+        trace: true, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ]
 };
